fbec8e7386f5b635ed207d337333d7c77db610dd
Ömer Balyali <omerbalyali@gmail.com>
2021-11-20 18:51:49 +0100

    Update nvm version, install node LTS version instead of the latest

--- development.sh
+++ development.sh
@@ -1,10 +1,10 @@
 # Install NVM
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
 
-# Install Node
-nvm install node
-nvm alias default node
-nvm use node
+# Install Node LTS
+nvm install --lts
+nvm alias default 'lts/*'
+nvm use default
 
 # Update npm
 npm install -g npm@latest
@@ -12,8 +12,17 @@ npm install -g npm@latest
 # Install pnpm
 curl -L https://raw.githubusercontent.com/pnpm/self-installer/master/install.js | node
 
+# Install Rush
+pnpm add --global @microsoft/rush
+
+# Install Rush
+pnpm add --global typescript
+
 # Install ESLint
-pnpm add --global eslint
+# pnpm add --global eslint
 
 # Install Prettier
-pnpm add --global prettier
+# pnpm add --global prettier
+
+
+
