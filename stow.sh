e7ce86029332b9baabd80645811c6ebf22961c47
Ömer Balyali <omerbalyali@gmail.com>
2022-05-04 15:08:15 +0200

    Remove alias logic

--- stow.sh
+++ stow.sh
@@ -23,14 +23,6 @@ echo '🟡 Adding shell aliases:'
 #   echo "\n GENERIC_ALIASES=true \n" >> ./zsh/.zshrc
 # fi
 
-if $personalized
-then
-  echo "\n" >> ./zsh/.zshrc
-  cat "./zsh/aliases/personalized.sh" >> ./zsh/.zshrc
-else
-  echo "\n" >> ./zsh/.zshrc
-  cat "./zsh/aliases/generic.sh" >> ./zsh/.zshrc
-fi
 
 echo '\n'
 echo '🟢 Config files are stowed/linked.'
