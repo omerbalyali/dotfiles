91251345e213e709bcb0df67f370ca9a30ef93e7
Ömer Balyali <omerbalyali@gmail.com>
2022-12-06 19:18:04 +0100

    Add prepare script

---
+++ prepare.sh
@@ -0,0 +1,11 @@
+# Install all remaining updates, if applicable
+sudo softwareupdate -i -a
+
+# Install Apple Command Line Developer Tools
+xcode-select --install
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
